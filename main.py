e7941f8d61ad949c1447bbbb4327f060a101333a
Anshu Kumar <anshukumar@Anshus-MacBook.local>
2023-05-22 22:42:57 +0530

    Added some features

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import face_recognition
 import os
 from datetime import datetime
 
-# from PIL import ImageGrab
 path = 'Training_images'
 images = []
 classNames = []
@@ -16,66 +15,69 @@ for cl in myList:
     classNames.append(os.path.splitext(cl)[0])
 print(classNames)
 
+# Compute face encodings for known images
+knownEncodings = []
+knownNames = []
+for image, name in zip(images, classNames):
+    rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    boxes = face_recognition.face_locations(rgb, model='hog')
+    encodings = face_recognition.face_encodings(rgb, boxes)
+    for encoding in encodings:
+        knownEncodings.append(encoding)
+        knownNames.append(name)
 
-def findEncodings(images):
-    encodeList = []
-
-
-    for img in images:
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        encode = face_recognition.face_encodings(img)[0]
-        encodeList.append(encode)
-    return encodeList
-
-
-def markAttendance(name):
-    with open('Attendance.csv', 'r+') as f:
-        myDataList = f.readlines()
-
-
-        nameList = []
-        for line in myDataList:
-            entry = line.split(',')
-            nameList.append(entry[0])
-            if name not in nameList:
-                now = datetime.now()
-                dtString = now.strftime('%H:%M:%S')
-                f.writelines(f'\n{name},{dtString}')
-
-#### FOR CAPTURING SCREEN RATHER THAN WEBCAM
-# def captureScreen(bbox=(300,300,690+300,530+300)):
-#     capScr = np.array(ImageGrab.grab(bbox))
-#     capScr = cv2.cvtColor(capScr, cv2.COLOR_RGB2BGR)
-#     return capScr
-
-encodeListKnown = findEncodings(images)
 print('Encoding Complete')
 
 cap = cv2.VideoCapture(0)
 
 while True:
-    success, img = cap.read()
-# img = captureScreen()
-    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
-    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+    # Capture frame-by-frame
+    ret, frame = cap.read()
 
-    facesCurFrame = face_recognition.face_locations(imgS)
-    encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+    # Convert the frame from BGR to RGB for face recognition
+    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
-        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
-        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
-# print(faceDis)
-        matchIndex = np.argmin(faceDis)
+    # Find all the faces and face encodings in the current frame of video
+    boxes = face_recognition.face_locations(rgb, model='hog')
+    encodings = face_recognition.face_encodings(rgb, boxes)
 
-        if matches[matchIndex]:
-            name = classNames[matchIndex].upper()
-            y1, x2, y2, x1 = faceLoc
-            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
-            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
-            cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
-            markAttendance(name)
+    # Loop over the face detections
+    for box, encoding in zip(boxes, encodings):
+        # See if the face is a match for the known face(s)
+        matches = face_recognition.compare_faces(knownEncodings, encoding)
+        name = "NOT_FOUND"  # Default name if face is not recognized
 
-    cv2.imshow('Webcam', img)
-    cv2.waitKey(1)
+        # Find the best match
+        faceDistances = face_recognition.face_distance(knownEncodings, encoding)
+        bestMatchIndex = np.argmin(faceDistances)
+        if matches[bestMatchIndex]:
+            name = knownNames[bestMatchIndex]
+
+        # Draw a box around the face
+        top, right, bottom, left = box
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
+
+        # Draw a label with the name below the face
+        cv2.rectangle(frame, (left, bottom - 25), (right, bottom), (0, 255, 0), cv2.FILLED)
+        cv2.putText(frame, name, (left + 6, bottom - 6), cv2.FONT_HERSHEY_DUPLEX, 0.5, (255, 255, 255), 1)
+
+        # Mark attendance if the person is recognized
+        if name != "NOT_FOUND":
+            with open('Attendance.csv', 'r+') as f:
+                attendanceData = f.read()
+                if name not in attendanceData:
+                    now = datetime.now()
+                    dtString = now.strftime('%H:%M:%S')
+                    f.write(f'\n{name},{dtString}')
+
+    # Display the resulting image
+    cv2.imshow('Video', frame)
+
+    # Quit the program when 'q' is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the capture
+cap.release()
+cv2.destroyAllWindows()
+
